e3d3d0e758974c7ae016a05418c4eadd21d826d1
Alex Archambault <alexandre.archambault@gmail.com>
2025-12-05 01:35:45 +0100

    Hard-code actual Mill dev branch commit

--- .github/scripts/setup-dev-mill.sh
+++ .github/scripts/setup-dev-mill.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+MILL_COMMIT="3c5e636923ab"
+
 git clone https://github.com/alexarchambault/mill.git -b scala3 mill-scala3
 cd mill-scala3
+git switch --detach "$MILL_COMMIT"
 ./mill -i dist.installLocal
 cd ..
 
