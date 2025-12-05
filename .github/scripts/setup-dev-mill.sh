24f0fc05ba0abbf8962d57bc68a3941aae80886d
Alex Archambault <alexandre.archambault@gmail.com>
2025-12-05 01:52:32 +0100

    ?

--- .github/scripts/setup-dev-mill.sh
+++ .github/scripts/setup-dev-mill.sh
@@ -3,10 +3,13 @@ set -e
 
 MILL_COMMIT="7be4725cfd"
 
+mkdir thing
+cd thing
 git clone https://github.com/alexarchambault/mill.git -b scala3 mill-scala3
 cd mill-scala3
 git switch --detach "$MILL_COMMIT"
 ./mill -i dist.installLocal
 cd ..
+cd ..
 
-ln -sf mill-scala3/mill-assembly.jar mill
+ln -sf thing/mill-scala3/mill-assembly.jar mill
